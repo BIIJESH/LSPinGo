c54a817d1d923bdfcbe0cf2ff290233b92a7a994
BIIJESH <BIIJESH@proton.me>
2025-02-13 22:40:41 +0545

    rpc decode tst

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,8 +1,11 @@
 package rpc
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
 )
 
 func EncodeMessage(msg any) string {
@@ -11,4 +14,19 @@ func EncodeMessage(msg any) string {
 		panic(err)
 	}
 	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+func DecodeMessage(msg []byte) (int, error) {
+	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return 0, errors.New("Did not find seperator")
+	}
+	//Content-Length; <number>
+	contentLengthBytes := header[len("Content-Length: "):]
+	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+	if err != nil {
+		return 0, err
+	}
+	_ = content
+	return contentLength, nil
 }
