1fe9ce54779be70fae5ff8b6176ad8cd76059b97
BIIJESH <BIIJESH@proton.me>
2025-02-16 20:52:53 +0545

    completed diagnostics with static test

--- main.go
+++ main.go
@@ -13,11 +13,14 @@ import (
 
 func main() {
 	logger := getLogger("/home/papa/educationalsp/log.txt")
-	logger.Println("this started")
+	logger.Println("Hey, I started!")
+
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
+
 	state := analysis.NewState()
 	writer := os.Stdout
+
 	for scanner.Scan() {
 		msg := scanner.Bytes()
 		method, contents, err := rpc.DecodeMessage(msg)
@@ -25,38 +28,69 @@ func main() {
 			logger.Printf("Got an error: %s", err)
 			continue
 		}
+
 		handleMessage(logger, writer, state, method, contents)
 	}
 }
+
 func handleMessage(logger *log.Logger, writer io.Writer, state analysis.State, method string, contents []byte) {
 	logger.Printf("Received msg with method: %s", method)
+
 	switch method {
 	case "initialize":
 		var request lsp.InitializeRequest
 		if err := json.Unmarshal(contents, &request); err != nil {
-			logger.Printf("Hey couldn't parse this: %s", err)
+			logger.Printf("Hey, we couldn't parse this: %s", err)
 		}
-		logger.Printf("Conntected to :%s %s", request.Params.ClientInfo.Name, request.Params.ClientInfo.Version)
+
+		logger.Printf("Connected to: %s %s",
+			request.Params.ClientInfo.Name,
+			request.Params.ClientInfo.Version)
+
+		// hey... let's reply!
 		msg := lsp.NewInitializeResponse(request.ID)
 		writeResponse(writer, msg)
-		logger.Print("Sent the reply")
 
+		logger.Print("Sent the reply")
 	case "textDocument/didOpen":
 		var request lsp.DidOpenTextDocumentNotification
 		if err := json.Unmarshal(contents, &request); err != nil {
-			logger.Printf("textDocument/didOpen %s", err)
+			logger.Printf("textDocument/didOpen: %s", err)
+			return
 		}
-		logger.Printf("Opened: %s ", request.Params.TextDocument.URI)
-		state.OpenDocument(request.Params.TextDocument.URI, request.Params.TextDocument.Text)
+
+		logger.Printf("Opened: %s", request.Params.TextDocument.URI)
+		diagnostics := state.OpenDocument(request.Params.TextDocument.URI, request.Params.TextDocument.Text)
+		writeResponse(writer, lsp.PublishDiagnosticsNotification{
+			Notification: lsp.Notification{
+				RPC:    "2.0",
+				Method: "textDocument/publishDiagnostics",
+			},
+			Params: lsp.PublishDiagnosticsParams{
+				URI:         request.Params.TextDocument.URI,
+				Diagnostics: diagnostics,
+			},
+		})
 	case "textDocument/didChange":
 		var request lsp.TextDocumentDidChangeNotification
 		if err := json.Unmarshal(contents, &request); err != nil {
 			logger.Printf("textDocument/didChange: %s", err)
 			return
 		}
+
 		logger.Printf("Changed: %s", request.Params.TextDocument.URI)
 		for _, change := range request.Params.ContentChanges {
-			state.UpdateDocument(request.Params.TextDocument.URI, change.Text)
+			diagnostics := state.UpdateDocument(request.Params.TextDocument.URI, change.Text)
+			writeResponse(writer, lsp.PublishDiagnosticsNotification{
+				Notification: lsp.Notification{
+					RPC:    "2.0",
+					Method: "textDocument/publishDiagnostics",
+				},
+				Params: lsp.PublishDiagnosticsParams{
+					URI:         request.Params.TextDocument.URI,
+					Diagnostics: diagnostics,
+				},
+			})
 		}
 	case "textDocument/hover":
 		var request lsp.HoverRequest
@@ -64,29 +98,47 @@ func handleMessage(logger *log.Logger, writer io.Writer, state analysis.State, m
 			logger.Printf("textDocument/hover: %s", err)
 			return
 		}
-		//Create a response and write it back
+
+		// Create a response
 		response := state.Hover(request.ID, request.Params.TextDocument.URI, request.Params.Position)
+
+		// Write it back
 		writeResponse(writer, response)
-	case "textDocument/definition": //TODO:write the definition better thinking of highlighting the matching strings
+	case "textDocument/definition":
 		var request lsp.DefinitionRequest
 		if err := json.Unmarshal(contents, &request); err != nil {
 			logger.Printf("textDocument/definition: %s", err)
 			return
 		}
+
+		// Create a response
+		response := state.Definition(request.ID, request.Params.TextDocument.URI, request.Params.Position)
+
+		// Write it back
+		writeResponse(writer, response)
 	case "textDocument/codeAction":
 		var request lsp.CodeActionRequest
 		if err := json.Unmarshal(contents, &request); err != nil {
 			logger.Printf("textDocument/codeAction: %s", err)
 			return
 		}
+
+		// Create a response
+		response := state.TextDocumentCodeAction(request.ID, request.Params.TextDocument.URI)
+
+		// Write it back
+		writeResponse(writer, response)
 	case "textDocument/completion":
 		var request lsp.CompletionRequest
 		if err := json.Unmarshal(contents, &request); err != nil {
-			logger.Printf("textDocument/completion: %s", err)
+			logger.Printf("textDocument/codeAction: %s", err)
 			return
 		}
-		//Create a response and write it back
+
+		// Create a response
 		response := state.TextDocumentCompletion(request.ID, request.Params.TextDocument.URI)
+
+		// Write it back
 		writeResponse(writer, response)
 	}
 }
@@ -94,13 +146,14 @@ func handleMessage(logger *log.Logger, writer io.Writer, state analysis.State, m
 func writeResponse(writer io.Writer, msg any) {
 	reply := rpc.EncodeMessage(msg)
 	writer.Write([]byte(reply))
+
 }
+
 func getLogger(filename string) *log.Logger {
 	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
 	if err != nil {
-		panic("You didn't give me a new file")
+		panic("hey, you didnt give me a good file")
 	}
 	return log.New(logfile, "[educationalsp]", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
-//TODO:from 1:40:13
